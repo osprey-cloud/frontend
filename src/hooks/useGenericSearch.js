adccd83ce2b7b9f48c1c9ef84db63c4afab9e181
rhodinemma <rhodinemma10@gmail.com>
2024-11-11 09:32:19 +0300

    chore: add endpoint to search through apps

--- src/hooks/useGenericSearch.js
+++ src/hooks/useGenericSearch.js
@@ -1,13 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import api from './../axios';
+import api from "./../axios";
 
 export const useGenericSearch = (keywords) => {
-    return useQuery({
-      queryFn: () => api.get(`/search?keywords=${keywords}`),
-      queryKey: ["generic_search"],
-      meta: {
-        errorMessage: "Failed to fetch resources"
-      }
-      
-    });
-  }
+  return useQuery({
+    queryFn: () => api.get(`/search?keywords=${keywords}`),
+    queryKey: ["generic_search"],
+    meta: {
+      errorMessage: "Failed to fetch resources",
+    },
+  });
+};
